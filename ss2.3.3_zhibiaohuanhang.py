a9928cbe688c16facdaa754c0b8e9685716f37c6
HerrXu01 <xuzt.work@gmail.com>
2021-08-18 11:06:22 +0800

    ss2.3.3_zhibiaohuanhang

---
+++ ss2.3.3_zhibiaohuanhang.py
@@ -0,0 +1,13 @@
+#2.3.3 使用制表符或换行符来添加空白
+
+#没有空白的输出
+print("python")
+
+#添加制表符，用\t
+print("\tpython")
+
+#添加换行符，用\n
+print("Languages:\nPython\nC\nJavaScript")
+
+#同一字符串中可以同时包含制表符和换行符
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
