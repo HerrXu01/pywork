72e10aadb32808e57203dfafdb2ca73439fc1543
HerrXu01 <xuzt.work@gmail.com>
2021-08-18 16:22:21 +0800

    practice2-6

---
+++ prac2_6.py
@@ -0,0 +1,8 @@
+#名言2
+#重复2-5，但是将名人的姓名存在变量famous_person中
+#再创建要显示的信息，并将其存储在message中，打印出来
+
+famous_person = "Albert Einstein"
+message = famous_person + 'once said, \
+"A person who never made a mistake never tried anything new."'
+print(message)
