e7eacf5b199a48e8b5b875f16c81200a38516d10
HerrXu01 <xuzt.work@gmail.com>
2021-08-18 15:38:27 +0800

    practice2-5

---
+++ prac2_5.py
@@ -0,0 +1,8 @@
+#名言
+#找一句名言，将姓名和名言打印出来
+# Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+print('Albert Einstein once said,\
+ "A person who never made a mistake never tried anything new."')
+
+#字符串太长可以分行来写，在上一行结尾加上\
