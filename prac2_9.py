e8dd0d7157a725fc4a7b1ea27f6c7c924ab9d4f3
HerrXu01 <xuzt.work@gmail.com>
2021-08-18 17:18:49 +0800

    practice 2-9

---
+++ prac2_9.py
@@ -0,0 +1,6 @@
+#最喜欢的数字
+#将最喜欢的数字存储在一个变量中，再用这个变量创建一条消息，并打印出来
+
+favorite_number = int(input("What's your favorite integer?\n"))
+message = "Your favorite integer is " + str(favorite_number) + "."
+print(message)
