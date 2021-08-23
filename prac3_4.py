ff2dcef00a217a355424b575b0d4e3b23a09f715
HerrXu01 <xuzt.work@gmail.com>
2021-08-23 16:33:35 +0800

    practice 3-4

---
+++ prac3_4.py
@@ -0,0 +1,8 @@
+#practice 3-4
+#邀请三个人共进晚餐，创建一个列表，包含三个人的名字
+#使用这个列表打印消息
+
+guest = ['Boliang', 'Mingzhe', 'Qianyue']
+message = 'I want to invite ' + guest[0] + ', '\
+			+ guest[1] + ', ' + guest[2] + ' for dinner.'
+print(message)
